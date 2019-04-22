a59daa287541944493d72cc666bf066273c23959
Amit Chowdhury <amitchowdhury@Amits-MacBook-Pro-2.local>
2019-04-22 21:53:35 +0600

    Navigation issue fix

---
+++ Aurora/Model/ProductDto.swift
@@ -0,0 +1,13 @@
+//
+//  ProductDto.swift
+//  Aurora
+//
+//  Created by Amit Chowdhury on 4/18/19.
+//  Copyright © 2019 Amit Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ProductDto: NSObject {
+
+}
